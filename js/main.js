bd1f97d6e0df35af258edf5fd229d6bf95ce99ce
Ryan O'Keefe <Ryan O'Keefe>
2017-05-13 16:39:20 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,21 +2,45 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
+//////////////////
 
-var userFlipped = [cardOne, cardTwo]
+var checkForMatch = function() {
+		if (cardsInPlay.length === 2);
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match!");
+		}
+		else {
+			alert("Sorry, try again.");
+		};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+};
+
+/////////////////
+
+var flipCard = function(cardId) {
 
 
-window.alert("User flipped" + " " + cardsInPlay);
 
-if (cardsInPlay.length === 2);
-if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
+	//	var cardOne = cards[0];
+	//	var cardTwo = cards[1];
+
+	//	var userFlipped = [cardOne, cardTwo]
+
+	//	cardsInPlay.push(cardOne);
+	//	cardsInPlay.push(cardTwo);
+
+
+	//	window.alert("User flipped" + " " + cardsInPlay);
+
+	console.log(alert("User flipped " + cards[cardId]))
+		
+		cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+
+};
+
+flipCard(3);
+
+flipCard(2);
+
